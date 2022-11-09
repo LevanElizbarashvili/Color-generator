ac0c71010b993af329c1c402f1f9b9cf2daf506f
LevanElizbarashvili <92277115+LevanElizbarashvili@users.noreply.github.com>
2022-11-10 02:54:46 +0400

    1.0

---
+++ index.js
@@ -0,0 +1,26 @@
+const hexId = document.getElementById("clr").value.slice(1);
+const modes = document.getElementById("schemes").value;
+const generatedColors = document.getElementById("clrs");
+const btn = document.getElementById("color-btn");
+
+fetch(`https://www.thecolorapi.com/scheme?hex=${hexId}&mode=${modes}&count=5`)
+  .then((res) => res.json())
+  .then((data) => render(data));
+
+function render(data) {
+  let html = "";
+  const colorArr = data.colors;
+  for (let i = 0; i < colorArr.length; i++) {
+    html += `<div class="container">
+      <div class="color" style="background-color: ${data.colors[i].hex.value}"></div>
+      <p>${data.colors[i].hex.value}</p></div>`;
+  }
+  generatedColors.innerHTML = html;
+}
+
+btn.addEventListener("click", function (event) {
+  event.preventDefault();
+  location.reload();
+});
+
+// test
