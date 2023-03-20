cb6520a9afaf651e3834d481cdd8ad36d0c0c29e
LevanElizbarashvili <92277115+LevanElizbarashvili@users.noreply.github.com>
2023-03-21 02:27:35 +0400

    updated reRender

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const hexId = document.getElementById("clr").value.slice(1);
-const modes = document.getElementById("schemes").value;
 const generatedColors = document.getElementById("clrs");
 const btn = document.getElementById("color-btn");
 
-fetch(`https://www.thecolorapi.com/scheme?hex=${hexId}&mode=${modes}&count=5`)
-  .then((res) => res.json())
-  .then((data) => render(data));
+function main() {
+  const hexId = document.getElementById("clr").value.slice(1);
+  const modes = document.getElementById("schemes").value;
+  const url = `https://www.thecolorapi.com/scheme?hex=${hexId}&mode=${modes}&count=5`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => render(data));
+}
 
-function render(data) {
+function render(clrData) {
   let html = "";
-  const colorArr = data.colors;
+  const colorArr = clrData.colors;
   for (let i = 0; i < colorArr.length; i++) {
     html += `<div class="container">
-      <div class="color" style="background-color: ${data.colors[i].hex.value}"></div>
-      <p>${data.colors[i].hex.value}</p></div>`;
+    <div class="color" style="background-color: ${clrData.colors[i].hex.value}"></div>
+    <p>${clrData.colors[i].hex.value}</p></div>`;
   }
   generatedColors.innerHTML = html;
 }
 
-btn.addEventListener("click", function (event) {
-  event.preventDefault();
-  location.reload();
-});
+function reRender() {
+  main();
+}
+
+btn.addEventListener("click", reRender);
